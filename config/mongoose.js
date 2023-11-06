75839c2726ee68e941ba2788109b89f11d6a2d8a
krishnaUrkude <140192785+krishnaUrkude@users.noreply.github.com>
2023-11-06 15:23:29 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const url = 'mongodb://0.0.0.0:27017/db';
+const url = process.env.MONGO_URL;
 // mongoose.connect('url') 
 mongoose.connect(url);
 const db=mongoose.connection;
